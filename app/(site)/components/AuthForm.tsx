ef436416373c149f6cd4cffb306d626a19988815
adityagarg2614 <168133722+adityagarg2614@users.noreply.github.com>
2026-03-08 05:07:51 +0530

    added Input box and authForm

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,73 @@
+'use client'
+import Button from '@/app/components/Button';
+import Input from '@/app/components/inputs/Input';
+import React, { useCallback, useState } from 'react'
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+
+
+type Variant = 'LOGIN' | 'REGISTER'
+
+const AuthForm = () => {
+    const [variant, setVariant] = useState<Variant>('LOGIN');
+    const [isLoading, setIsLoading] = useState(false);
+    const toggleVariant = useCallback(()=>{
+        if(variant == 'LOGIN'){
+            setVariant('REGISTER');
+        }else{
+            setVariant('LOGIN');
+        }
+    },[variant]);
+
+   const{
+    register,
+    handleSubmit,
+    formState:{
+        errors
+    }
+   } = useForm<FieldValues>({
+    defaultValues:{
+        name:"",
+        email:"",
+        password:""
+    }
+   })
+
+   const onSubmit: SubmitHandler<FieldValues> = (data) =>{
+        setIsLoading(true);
+        if(variant == 'REGISTER'){
+            // axios Register
+        }
+        if(variant == 'LOGIN'){
+            // Next auth sign in 
+        }
+   }
+
+   const socialAction = (action:string)=>{
+    setIsLoading(true);
+    // next auth social sigin
+   }
+
+  return (
+    <div
+    className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
+      <div
+      className='bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10'>
+        <form 
+        className='space-y-6 '
+        onSubmit={handleSubmit(onSubmit)}>
+
+            {variant == 'REGISTER' && (
+                <Input id='name' label='Name' register={register} errors={errors}/>
+            )}
+            <Input id='email' label='Email Address' type='email' register={register} errors={errors}/>
+            <Input id='password' label='Password' type='password' register={register} errors={errors}/>
+            <div>
+                <Button> Test</Button>
+            </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default AuthForm
