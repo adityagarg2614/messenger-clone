09260c84df7720b7c8f7032b5cc832d1c7cfd602
adityagarg2614 <168133722+adityagarg2614@users.noreply.github.com>
2026-03-09 08:15:40 +0530

    added ToasterContext from react hot toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ToasterContext from "./context/ToasterContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,9 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ToasterContext/>
         {children}
       </body>
     </html>
