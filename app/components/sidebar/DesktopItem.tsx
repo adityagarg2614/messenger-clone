44460f8860817e9fc782ea7dd1091820a6ae8a1f
adityagarg2614 <168133722+adityagarg2614@users.noreply.github.com>
2026-03-10 12:01:06 +0530

    created DesktopSidebar.tsx

---
+++ app/components/sidebar/DesktopItem.tsx
@@ -0,0 +1,40 @@
+'use client'
+import React from 'react'
+import clsx from 'clsx'
+import Link from 'next/link'
+
+interface DesktopItemProps{
+    label: string;
+    icon: any;
+    href: string;
+    onClick?: () => void;
+    active?: boolean;
+
+}
+
+const DesktopItem: React.FC<DesktopItemProps> = ({
+    label,
+    icon,
+    href,
+    onClick,
+    active
+}) => {
+
+    const handleClick = () => {
+        if(onClick){
+            return onClick();
+        }
+    };
+
+  return (
+    <li onClick={handleClick}>
+        <Link href={href}> 
+            <span>
+                {label}
+            </span>
+        </Link>
+    </li>
+  )
+}
+
+export default DesktopItem
