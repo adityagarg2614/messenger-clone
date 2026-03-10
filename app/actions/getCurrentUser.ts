935823acdbf041c6dc676d4e83334b9f2e25faa9
adityagarg2614 <168133722+adityagarg2614@users.noreply.github.com>
2026-03-11 04:44:14 +0530

    created actions getSession and getCurrentUser

---
+++ app/actions/getCurrentUser.ts
@@ -0,0 +1,22 @@
+import prisma from "../libs/prisma";
+import getSession from "./getSession";
+
+export default async function getCurrentUser() {
+    try {
+        const session = await getSession();
+        if (!session?.user?.email) {
+            return null;
+        }
+        const user = await prisma.user.findUnique({
+            where: {
+                email: session.user.email
+            }
+        })
+        if (!user) {
+            return null;
+        }
+        return user;
+    } catch (error) {
+        return null;
+    }
+}
