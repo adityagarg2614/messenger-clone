8c4e38c0f93f4de15fbc620242855129c8ea9f1e
adityagarg2614 <168133722+adityagarg2614@users.noreply.github.com>
2026-03-13 00:27:04 +0530

    Adds error logging to conversation retrieval, includes type assertion for current user email, and applies minor formatting adjustments.

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -15,7 +15,7 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
 
 
   return (
-    <div className='hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-auto lg:bg-white lg:border-r lg:pb-4 lg:flex lg:flex-col justify-between '>
+    <div className='hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-auto lg:bg-white lg:border-r lg:pb-4 lg:flex lg:flex-col justify-between'>
       <nav className='mt-4 flex flex-col justify-between'>
         <ul role='list' className='flex flex-col items-center space-y-1'>
           {routes.map((item) => (
