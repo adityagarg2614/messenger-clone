1276cfd5886e117737064c7761b796f2a7be393d
adityagarg2614 <adityagarg065@gmail.com>
2026-03-16 03:07:00 +0530

    modifing it mroe and adding features to it

---
+++ app/components/inputs/Select.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+interface SelectProps {
+    label: string;
+    value?: Record<string, any>;
+    onChange: (value: Record<string, any>) => void;
+    options: Record<string, any>[];
+    disabled?: boolean;
+}
+
+
+const Select: React.FC<SelectProps> = ({ label, value, onChange, options, disabled }) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+export default Select
