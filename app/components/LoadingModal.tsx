33b82a1e6629d4c72048840d4be5432fa8e1ca9f
adityagarg2614 <adityagarg065@gmail.com>
2026-03-17 05:10:12 +0530

    refactor: Simplify `LoadingModal`'s layout and integrate it into `UserBox` to display a loading state when a user is clicked.

--- app/components/LoadingModal.tsx
+++ app/components/LoadingModal.tsx
@@ -9,25 +9,14 @@ const LoadingModal = () => {
         <Transition.Root show as={Fragment}>
             <Dialog as="div" className="relative z-50" onClose={() => { }}>
                 <Transition.Child as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in duration-200" leaveFrom="opacity-100" leaveTo="opacity-0">
-                    <div className="fixed inset-0 bg-gray-100 bg-opacity-50" />
+                    <div className="fixed inset-0 bg-gray-100 bg-opacity-50 transition-opacity" />
                 </Transition.Child>
-                <div className="fixed inset-0 overflow-hidden">
-                    <div className="absolute inset-0 overflow-hidden">
-                        <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
-                            <Transition.Child as={Fragment} enter="transform transition ease-in-out duration-500" enterFrom="translate-x-full" enterTo="translate-x-0" leave="transform transition ease-in-out duration-500" leaveFrom="translate-x-0" leaveTo="translate-x-full">
-                                <Dialog.Panel className="pointer-events-auto w-screen max-w-md">
-                                    <div className="flex h-full flex-col overflow-y-scroll bg-white py-6 shadow-xl">
-                                        <div className="relative mt-6 flex-1 px-4 sm:px-6">
-                                            <div className="absolute inset-0">
-                                                <div className="flex justify-center items-center h-full">
-                                                    <ClipLoader size={40} color="#06b6d4" />
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </Dialog.Panel>
-                            </Transition.Child>
-                        </div>
+                <div className="fixed inset-0 z-10 overflow-hidden">
+                    <div className="flex min-h-full items-center justify-center p-4 text-center">
+                        <Dialog.Panel>
+                            <ClipLoader size={40} color="#0284c7" />
+                        </Dialog.Panel>
+
                     </div>
                 </div>
             </Dialog>
